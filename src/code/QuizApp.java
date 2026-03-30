fd134f83f4bf93cbfb057a14ed8ca981dd25693b
Damon Cao <damoncaoxd@gmail.com>
2026-03-30 11:16:55 -0700

    Initial connection with backend logic

--- src/code/QuizApp.java
+++ src/code/QuizApp.java
@@ -6,49 +6,62 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-import java.awt.*;
-
 public class QuizApp extends Application {
+
+    private static final int SCENE_WIDTH = 800;
+    private static final int SCENE_HEIGHT = 450;
+    private static final int DEFAULT_SCORE = 0;
 
     @Override
     public void start(final Stage primaryStage)
     {
+        final Quiz quiz;
+        quiz = new Quiz();
 
-        VBox myVBox;
+        final VBox myVBox;
         myVBox = new VBox(10);
         myVBox.getStyleClass().add("vbox");
 
-        Button startButton;
+        final Button startButton;
         startButton = new Button("Start Quiz!");
         startButton.getStyleClass().add("button");
 
-        Label startLabel;
+        final Label startLabel;
         startLabel = new Label("Press 'Start Quiz' to begin!");
         startLabel.getStyleClass().add("label");
 
-        TextField answerField;
+        final Label questionField;
+        questionField = new Label(quiz.generateQuestion());
+
+        final TextField answerField;
         answerField = new TextField();
         answerField.setPromptText("Answer: ");
         answerField.getStyleClass().add("text-field");
 
-        Button submitButton;
+        final Button submitButton;
         submitButton = new Button("Submit!");
         submitButton.getStyleClass().add("button");
 
-        Label scoreLabel;
-        scoreLabel = new Label("Score: ");
+        final int score;
+        final Label scoreLabel;
+        score = DEFAULT_SCORE;
+        scoreLabel = new Label("Score: " + score);
         scoreLabel.getStyleClass().add("label");
 
 
         myVBox.getChildren().addAll(
                 startButton,
                 startLabel,
+                questionField,
                 answerField,
                 submitButton,
                 scoreLabel
         );
 
-        Scene scene = new Scene(myVBox, 200, 150);
+        final Scene scene;
+        scene = new Scene(myVBox,
+                        SCENE_WIDTH,
+                        SCENE_HEIGHT);
 
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
